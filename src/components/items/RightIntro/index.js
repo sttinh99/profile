2381dc013429939a8b04158871216c34c01c21fb
Quang Tịnh Trương <58225578+sttinh99@users.noreply.github.com>
2021-08-05 20:08:10 +0700

    test

--- src/components/items/RightIntro/index.js
+++ src/components/items/RightIntro/index.js
@@ -9,12 +9,13 @@ export default function RightIntro() {
             <div className="gido">
                 <img src={my} alt="avatar"></img>
             </div>
-            <div className='text-amination'>
-                <p className='t1'>sss sss ss sss sss ss ss sss sss s sss sss sss sss sss ssss s</p>
-                <p className='t2'>sss sss ss sss sss ss ss sss sss s sss sss sss sss sss ssss s ss</p>
-                <p className='t3'>sss sss ss sss sss ss ss sss sss s sss sss sss sss sss ssss s ss sss</p>
-                <p className='t4'>sss sss ss sss sss ss ss sss sss s sss sss sss sss sss ssss s ss sss</p>
-            </div>
+            {/* <div className='text-amination'>
+                <p className='t1'>qqqqqqqqqqqqqqqqqqqqqqqqqqqq</p>
+                <p className='t2'>pppppppppppppppppppppppppppp</p>
+                <p className='t3'>qqqqqqqqqqqqqqqqqqqqqqqqqqqq</p>
+                <p className='t4'>pppppppppppppppppppppppppppp</p>
+                <p className='t5'>================================</p>
+            </div> */}
         </div>
     );
 }
